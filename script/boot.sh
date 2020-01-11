daa97c071f8bb5e9b4c64420bf62b2be0988a3a2
ybs32 <hdecogtrexi557960@gmail.com>
2020-01-12 00:40:50 +0900

    b

--- script/boot.sh
+++ script/boot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-node /var/www/service/bin/www &
+/usr/bin/node /var/www/service/bin/www &
 
 OPTIONS="--no-sandbox --incognito  --ignore-certificate-errors --allow-running-insecure-content"
-google-chrome-stable $OPTIONS &
+/usr/bin/google-chrome-stable $OPTIONS &
