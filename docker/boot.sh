f8b99c495a5c29ff9045677335fdfab14a3a4a3c
ybs32 <hdecogtrexi557960@gmail.com>
2020-01-12 03:53:37 +0900

    nu

--- docker/boot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-node /var/www/service/bin/www &
-
-OPTIONS="--no-sandbox --incognito  --ignore-certificate-errors --allow-running-insecure-content"
-google-chrome-stable $OPTIONS &
--- script/boot.sh
+++ script/boot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/bin/node /var/www/service/bin/www &
+node /var/www/service/bin/www &
 
 OPTIONS="--no-sandbox --incognito  --ignore-certificate-errors --allow-running-insecure-content"
-/usr/bin/google-chrome-stable $OPTIONS &
+google-chrome-stable $OPTIONS &
