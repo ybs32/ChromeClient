a5765001a63c678f589a574d9ae9add701549d05
ybs32 <hdecogtrexi557960@gmail.com>
2020-01-12 04:33:36 +0900

    entry

--- script/boot.sh
+++ script/boot.sh
@@ -3,3 +3,6 @@ node /var/www/service/bin/www &
 
 OPTIONS="--no-sandbox --incognito  --ignore-certificate-errors --allow-running-insecure-content"
 google-chrome-stable $OPTIONS &
+
+# Keep container running
+tail -f /dev/null
