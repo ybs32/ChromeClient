7bd44a34a8d8e3e1052cbbb8dadc2c5944b4bf04
ybs32 <hdecogtrexi557960@gmail.com>
2020-01-19 22:26:57 +0900

    chrome options

--- script/boot.sh
+++ script/boot.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 node /var/www/service/bin/www &
 
-OPTIONS="--no-sandbox --incognito  --ignore-certificate-errors --allow-running-insecure-content"
-google-chrome-stable $OPTIONS &
+# Make chrome user-data directory
+mkdir -p /home/chrome/.config/google-chrome
+
+# Start google chrome
+GENERAL_OPTIONS="--incognito --no-default-browser-check"
+SECURITY_OPTIONS="--no-sandbox --ignore-certificate-errors --allow-running-insecure-content"
+STORAGE_OPTIONS="--disable-dev-shm-usage --user-data-dir=/home/chrome/.config/google-chrome"
+google-chrome-stable $GENERAL_OPTIONS $SECURITY_OPTIONS $STORAGE_OPTIONS &
 
 # Keep container running
 tail -f /dev/null
